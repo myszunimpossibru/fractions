def75509306db84a062fe7910b6ddcefdbc3c5b3
Maciej Dems <maciej.dems@p.lodz.pl>
2018-10-10 09:18:19 +0200

    Better tests

--- fraction.py
+++ fraction.py
@@ -1,32 +1,61 @@
-"""
-This file contains definition of a fraction class.
-
-You should put complete class here. It must be named `Fragion` and must have the following properties:
-
-- four basic mathematical operators defined;
-- elegant conversion to string in the form '3/2';
-- simplification and clean-up on construction: both attribute divided by the greatest common divisor
-  sign in the nominator, denominator not zero (ValueError should be raised in such case), both attributes
-  must be integers (ValueError if not),
-- method `value` returning float value of the fraction.
-"""
 from math import gcd
 
-
 class Fraction(object):
-    """
-    Fraction class.
-    """
 
-    def __init__(self, nom, denom):
-        self.nom = nom
-        self.denom = denom
+
+    denominator = None
+    nominator = None
+
+    
+    def __init__(self, nominator, denominator):
+
+
+
+        self.nominator = nominator
+        self.denominator = denominator
+        
+
+
+
+        if self.denominator == 0:
+            raise ValueError("denominator must not be zero.")
+        
+
+        divisor = gcd(self.nominator, self.denominator)
+
+
+        self.nominator = int(nominator//divisor)
+        self.denominator = int(denominator//divisor)
+        
+        if (self.denominator < 0):
+            self.denominator = -1 * self.denominator
+            self.nominator = -1 * self.nominator
 
     def __str__(self):
-        return f"{self.nom}/{self.denom}"
+        if self.denominator == 1:
+            return "{0.nominator}".format(self)
+        else:
+            return "{0.nominator}/{0.denominator}".format(self)
+    
+    def __mul__(self, number):
+        return Fraction(self.nominator * number.nominator, self.denominator * number.denominator)
+    
+    def __truediv__(self, number):
+        return Fraction(self.nominator * number.denominator, self.denominator * number.nominator)
+    
+    def __add__(self, number):
+        n1 = self.nominator * number.denominator
+        n2 = self.denominator * number.nominator
+        return Fraction(n1 + n2, self.denominator * number.denominator)
+    
+    def __sub__(self, number):
+        n1 = self.nominator * number.denominator
+        n2 = self.denominator * number.nominator
+        return Fraction(n1 - n2, self.denominator * number.denominator)
 
-    def __repr__(self):
-        return f"Fraction({self.nom}, {self.denom})"
+    def __eq__(self, number):
+        return self.nominator == number.nominator and self.denominator == number.denominator
 
-    def __eq__(self, other):
-        return self.nom == other.nom and self.denom == other.denom
+    def value(self):
+        return self.nominator/self.denominator
+		
